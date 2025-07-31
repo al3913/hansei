124e4a90c6bdfda1311b7c3be3073751bdaed66f
al3913 <al3913@g.rit.edu>
2025-07-31 17:19:01 -0400

    header responsiveness

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function Header(){
     return (
         <header className="flex w-full items-center justify-between m-12">
-            <Button variant="link" className="rounded-full font-tthoves text-2xl font-extrabold p-0" actionType="inProgress"> Hansei.GG </Button>
+            <Button variant="link" className="rounded-full font-tthoves text-xl sm:text-2xl font-extrabold p-0" actionType="inProgress"> Hansei.GG </Button>
             <Image
               src="mascot/mascot.svg"
               alt="Hansei Logo"
               width={50}
               height={50}
             />
-            <Button variant="secondary" className="rounded-full font-bold py-6 px-8" actionType="booking"> Let&apos;s Chat! </Button>
+            <Button variant="secondary" className="rounded-full font-bold py-6 px-8 text-xs sm:text-sm" actionType="booking"> Let&apos;s Chat! </Button>
           </header>
     );
 }
