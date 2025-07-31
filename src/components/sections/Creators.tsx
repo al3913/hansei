c6345d988ac91bf684f03383bc56b776b20e8298
al3913 <al3913@g.rit.edu>
2025-07-31 14:40:19 -0400

    finished typography styling for hero, mobile-side

--- src/components/sections/Creators.tsx
+++ src/components/sections/Creators.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Creators() {
     return(
-        <section className="relative flex flex-col ">
+        <section className="relative flex flex-col w-full justify-center items-center bg-[url('/creators-bg.svg')] bg-cover bg-no-repeat aspect-[1072/617]">
             <div className="md:absolute md:top-0 md:left-0 flex flex-col justify-center gap-2 text-start">
                 <h2>Creator</h2>
                 <h1>Roster.</h1>
@@ -11,13 +11,15 @@ export default function Creators() {
                     partnered with, past and present.
                 </p>
             </div>
-            <Image 
+            <div className="md:absolute md:bottom-0 md:right-0 w-9/10">
+                <Image 
                 src="/creators.svg"
                 alt="Creator Roster - Reepal, Cloud805, CuddleCore, MizziMie, AtomicMari, PeachieTech, Tori Pareno, BasiilLeaf, Papalobster, IHeartJustice, Rikognition, VeeDotMe, James Chen (Jchensor), HideTone"
                 width={1000}
                 height={500}
-                className="font-winnie"
+                className="w-full h-full object-contain"
             />
+            </div>
         </section>
     )
 }
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,9 +3,14 @@ import { Button } from "@/components/ui/button";
 export default function Hero() {
     return (
         <section className="bg-[url('/hero-box.svg')] w-full aspect-[1072/617] bg-contain bg-no-repeat bg-center flex flex-col justify-center items-center">
-            <h1 className="text-brandWhite text-3xl "> You Stream.</h1>
-            <h2 className="text-brandWhite font-moisette"> We Grow Your Youtube.</h2>
-            <Button className="mt-4 rounded-"actionType="booking"> See My Channel&apos;s Potential</Button>
+            <h1 className="text-brandWhite text-3xl font-black italic"> You Stream.</h1>
+            <h2 className="text-brandWhite text-2xl font-moisette font-extrabold"> We Grow Your Youtube.</h2>
+            <Button className="mt-4 rounded-"actionType="booking"> 
+                <p className="font-bold text-xs">
+                    See My Channel&apos;s 
+                    <span className="font-moisette font-extrabold"> Potential</span>
+                </p>
+            </Button>
         </section>
     );
 }
