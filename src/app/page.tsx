0e11df7e33443ab88468eb0e0a3606beb01fea39
al3913 <al3913@g.rit.edu>
2025-08-01 12:47:51 -0400

    line changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
     <div className="relative bg-brandWhite z-0">
       <RedLine className="top-225 scale-600 md:scale-150 md:top-250"/>
       <BlueLine className="top-430 scale-450 md:top-410 md:scale-170"/>
-      <RedLine className="top-610 scale-600 md:scale-150 -rotate-55 md:-rotate-15"/>
-      <BlueLine className="block md:hidden scale-450 md:scale-150 top-750 rotate-55"/>
+      <RedLine className="top-610 md:top-630 scale-600 md:scale-150 -rotate-15"/>
+      <BlueLine className="block md:hidden scale-550 top-800 rotate-15"/>
       <RedLine className="block md:hidden top-900 scale-450 "/>
-      <BlueLine className="hidden md:block md:scale-130 rotate-5 md:top-1050"/>
+      <BlueLine className="hidden md:block md:scale-115 rotate-5 md:top-1050"/>
       <YellowLine className="top-1025 scale-400 md:-left-200 md:top-815 md:scale-105"/>
       <YellowLine className="block md:hidden top-1520 scale-450 rotate-15 md:-left-200 md:top-780 md:scale-150"/>
       <div className="flex min-h-screen max-w-screen-lg flex-col items-center justify-between mx-auto gap-15 p-5 z-1">
