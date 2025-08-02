3faede347b435663f87070b3ad958f17c6f9027c
al3913 <al3913@g.rit.edu>
2025-08-02 17:38:29 -0400

    fix for twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Hansei.GG",
     description: "Helping creators make a living doing what they love.",
-    images: ["https://hansei.gg/mascot/mascot_.png"],
+    images: ["https://hansei.gg/mascot/mascot_.png?v=2"],
   },
   icons: {
     icon: "/mascot/mascot_.png",
