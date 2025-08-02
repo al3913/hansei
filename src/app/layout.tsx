0a6459a6e0b6ca53180a6f108224ac118e6fc62b
al3913 <al3913@g.rit.edu>
2025-08-02 16:52:10 -0400

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,26 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hansei.GG",
   description: "Hansei.GG - Helping creators make a living doing what they love",
-  openGraph: {
-    title: "Hansei.GG",
-    description: "Helping creators make a living doing what they love.",
-    images: [
-      {
-        url: "/mascot/mascot_.png", // Use a PNG or JPG, not SVG
-        width: 1200,
-        height: 630,
-        alt: "Hansei mascot",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Hansei.GG",
-    description: "Helping creators make a living doing what they love.",
-    images: ["/mascot/mascot_.png"],
-  },
+  // openGraph: {
+  //   title: "Hansei.GG",
+  //   description: "Helping creators make a living doing what they love.",
+  //   images: [
+  //     {
+  //       url: "/mascot/mascot_.png", // Use a PNG or JPG, not SVG
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "Hansei mascot",
+  //     },
+  //   ],
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Hansei.GG",
+  //   description: "Helping creators make a living doing what they love.",
+  //   images: ["/mascot/mascot_.png"],
+  // },
   icons: {
-    icon: "/mascot/mascot-note-taking-svg.svg",
+    icon: "/mascot/mascot_.png",
   },
 };
 
